ecfbb3049003768648737f055f41233d492e7bce
Vultumast <vultumast+github@gmail.com>
2025-03-09 18:31:45 -0400

    initial

---
+++ src/Guitar-Hero-3.cpp
@@ -0,0 +1,12 @@
+﻿// Guitar-Hero-3.cpp : Defines the entry point for the application.
+//
+
+#include "Guitar-Hero-3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
